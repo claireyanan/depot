8e0f439edbfb3ae4ee077181b9405aca55666285
claireyanan <gynfly@gmail.com>
2015-06-04 19:20:09 -0700

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,14 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title # generated original
+
+	validates :title, :description, :image_url, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i, 
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
+	
   def change
 		create_table :products do |t|
 			t.string :title
@@ -9,4 +18,5 @@ class Product < ActiveRecord::Base
   		t.timestamps
 		end 
 	end
+
 end
